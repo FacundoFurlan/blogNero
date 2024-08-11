ad8e81bd9c208e41d67c7e56e5c66d215cca5c0c
NeroWork <84593907+NeroWork@users.noreply.github.com>
2024-08-11 16:29:40 -0300

    loading to login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -8,6 +8,7 @@ export default function Page(){
     const [errorMessage, setErrorMessage] = useState(""); //This useState sees if there was an error on the sign in proccess and storage the message in case it happens
     const [errorCount, setErrorCount] = useState(0); 
     const [fontSize, setFontSize] = useState(""); 
+    const [loading, setLoading] = useState(false); 
 
     const router = useRouter(); //This useRouter is used to redirect the user to the front page in case they log in correctly
 
@@ -45,16 +46,30 @@ export default function Page(){
     return(
         <main className="min-w-full min-h-screen flex flex-col items-center min-h-[80vh]">
             <h2 className="text-2xl mb-6">Name yourself!</h2>
-            <form action={formAction} className="flex flex-col text-lg w-[80vw] md:w-[50vw] lg:w-[50vw] xl:w-[30vw] border card-bg px-8">
+            <form action={formAction} onSubmit={()=>{setLoading(true)}} className="flex flex-col text-lg w-[80vw] md:w-[50vw] lg:w-[50vw] xl:w-[30vw] border card-bg px-8">
                 <p className={`${fontSize} mt-4 ease-in-out duration-300 text-center underline underline-offset-2 text-amber-400`}>{errorMessage}</p>
                 <label className="self-center m-4" htmlFor="email">Email</label>
                 <input className="py-2 px-3 border focus:outline-none focus:outline-red-800 focus:outline-offset-0 input-bg rounded border-amber-400" type="email" placeholder="arthur69@gmail.com" name="email" />
                 <label className="self-center m-4" htmlFor="password">Password</label>
                 <input className="py-2 px-3 border focus:outline-none focus:outline-red-800 focus:outline-offset-0 input-bg rounded border-amber-400" type="password" placeholder="***********" name="password" />
-                <div className="flex my-10 flex-col lg:justify-between lg:flex-row">
-                    <button className="mb-6 lg:mb-0 border-2 border-red-800 bg-amber-400 font-extrabold text-nowrap rounded-xl max-w-fit self-center py-1 px-6 text-red-800" type="submit">Sign In</button> 
-                    <button className="font-normal rounded text-nowrap max-w-fit underline underline-offset-2 self-center py-2 text-white" type="submit">I forgot my password</button> 
-                </div>
+                {
+                    loading?
+                    <div className="flex my-10">
+                        <l-ring
+                            size="80"
+                            stroke="5"
+                            bg-opacity="0"
+                            speed="2"
+                            color="white" 
+                        ></l-ring>
+                    </div>
+                    :
+                    <div className="flex my-10 flex-col lg:justify-between lg:flex-row">
+                        <button className="mb-6 lg:mb-0 border-2 border-red-800 bg-amber-400 font-extrabold text-nowrap rounded-xl max-w-fit self-center py-1 px-6 text-red-800" type="submit">Sign In</button> 
+                        <button className="font-normal rounded text-nowrap max-w-fit underline underline-offset-2 self-center py-2 text-white" type="submit">I forgot my password</button> 
+                    </div>
+
+                }
             </form>
         </main>
     )
