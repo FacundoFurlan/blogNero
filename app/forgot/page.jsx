0d33f3d5398bfbba13511e7359c82e4483dd263b
NeroWork <84593907+NeroWork@users.noreply.github.com>
2025-01-22 20:04:49 -0300

    agregue un poco de margin

--- app/forgot/page.jsx
+++ app/forgot/page.jsx
@@ -48,7 +48,7 @@ export default function Page(){
     return(
         <main className="flex flex-col items-center">
             <h2 className="text-2xl mb-6">Confirm your eMail</h2>
-            <form action={formAction} onSubmit={()=>{setLoading(true)}} className="flex flex-col text-lg w-[80vw] md:w-[50vw] lg:w-[50vw] xl:w-[30vw] border card-bg px-8">
+            <form action={formAction} onSubmit={()=>{setLoading(true)}} className="flex flex-col text-lg w-[80vw] md:w-[50vw] lg:w-[50vw] xl:w-[30vw] border card-bg px-8 mb-4">
                 <p className={`${fontSize} mt-4 ease-in-out duration-300 text-center underline underline-offset-2 text-amber-400`}>{errorMessage}</p>
 
                 <label className="self-center m-4" htmlFor="email">Email</label>
