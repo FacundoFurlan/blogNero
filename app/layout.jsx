2e7c27c36d89558d9854172f8982bf1ccf9c0b0a
NeroWork <84593907+NeroWork@users.noreply.github.com>
2025-01-23 17:58:17 -0300

    darkmode yes or yes

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
 
 
   return (
-    <html lang="en">
+    <html className="dark" lang="en">
 
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
